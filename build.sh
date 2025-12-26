f5a718e8ff94f945898b6e03ba7abff644268bbc
KirillAkril <demchenkokir07@gmail.com>
2025-12-27 00:47:48 +0300

    New commit

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Build script for Render deployment
 
+pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
